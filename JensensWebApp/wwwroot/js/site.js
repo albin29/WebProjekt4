7d7dbc367b388b68dff4f0d6ff581ac19a768c59
FredrikH03 <fredrikholst03@hotmail.com>
2024-08-14 12:44:23 +0200

    added code to save the current darkmode state

--- JensensWebApp/wwwroot/js/site.js
+++ JensensWebApp/wwwroot/js/site.js
@@ -3,8 +3,19 @@
 
 // Write your JavaScript code.
 
+window.onload = function() {
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+    }
+}
+
 document.getElementById('changeBackground').addEventListener('click', function changeBackground(){
     var element = document.body;
 
     element.classList.toggle("dark-mode")
+
+    if(element.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+    else{localStorage.removeItem('dark-mode')}
 })
